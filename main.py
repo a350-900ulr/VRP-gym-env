f9e49233631e548c5261c4104d583a979a02e7dd
will amminger <willamminger@gmail.com>
2023-11-10 20:21:54 +0100

    some links

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# https://stackify.com/tensorflow-vs-pytorch-which-deep-learning-framework-is-right-for-you/
-# "In addition, its dynamic graph execution approach facilitates rapid prototyping, experimentation and debugging."
+places = 80  # locations to use, from 0-80
+packages = 200
+bikes = 20
 
+'''
+https://keras.io/examples/rl/ddpg_pendulum/
+https://stackoverflow.com/questions/46422845/what-is-the-way-to-understand-proximal-policy-optimization-algorithm-in-rl#50663200
+file:///home/me/Documents/tuw/23W/Applied%20Deep%20Learning/ass1/Solving%20the%20vehicle%20routing%20problem%20with%20deep%20reinforcement%20learning2208.00202.pdf
+
+idk what im tryna figure out
+'''
