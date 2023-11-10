f9e213ab67601f310fc032392e0055a29304d5bd
will amminger <willamminger@gmail.com>
2023-11-10 17:25:33 +0100

    create .gitignore, add main file

---
+++ main.py
@@ -0,0 +1,3 @@
+# https://stackify.com/tensorflow-vs-pytorch-which-deep-learning-framework-is-right-for-you/
+# "In addition, its dynamic graph execution approach facilitates rapid prototyping, experimentation and debugging."
+
